e1f74873830553138d1ccc623ae88d8cf651887a
lucas-m12 <lucasmarinhodasilva2@gmail.com>
2024-07-14 12:43:03 -0300

    remove expire jwt

--- src/shared/auth/auth.module.ts
+++ src/shared/auth/auth.module.ts
@@ -13,7 +13,6 @@ import { LocalStrategy } from './local.strategy';
     PassportModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET || 'secret',
-      signOptions: { expiresIn: '1d' },
     }),
   ],
   providers: [AuthService, JwtStrategy, LocalStrategy],
