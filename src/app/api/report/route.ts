a67d9ee8053e8bf2c653610f1fa42067204ffd11
lucas-m12 <lucasmarinhodasilva2@gmail.com>
2024-09-21 11:54:14 -0300

    add response

--- src/app/api/report/route.ts
+++ src/app/api/report/route.ts
@@ -10,6 +10,7 @@ export const GET = async (request: NextRequest) => {
         "Content-Type": "application/pdf"
       }
     });
+    return new Response('OK', { status: 200 });
   } catch (error) {
     console.error("Error downloading PDF:", error);
   }
