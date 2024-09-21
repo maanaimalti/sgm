0d50731f6307ff45f22309d81986bafe43d43017
lucas-m12 <lucasmarinhodasilva2@gmail.com>
2024-09-21 14:20:50 -0300

    rename query param

--- src/app/api/report/route.ts
+++ src/app/api/report/route.ts
@@ -2,7 +2,7 @@ import type { NextRequest } from "next/server";
 
 export const GET = async (request: NextRequest) => {
   const searchParams = request.nextUrl.searchParams;
-  const pdfUrl = searchParams.get("url");
+  const pdfUrl = searchParams.get("urlpdf");
   console.log({ pdfUrl });
   if (!pdfUrl) {
     return new Response("Missing PDF URL", { status: 400 });
