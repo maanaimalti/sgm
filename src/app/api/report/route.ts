4aee5f729c54933466e23abee24242bc016c580b
lucas-m12 <lucasmarinhodasilva2@gmail.com>
2024-09-21 12:01:34 -0300

    add redirect

--- src/app/api/report/route.ts
+++ src/app/api/report/route.ts
@@ -1,16 +1,11 @@
-import axios from "axios";
+import { redirect } from "next/navigation";
 import { type NextRequest, NextResponse } from "next/server";
 
 export const GET = async (request: NextRequest) => {
   try {
     const searchParams = request.nextUrl.searchParams;
     const pdfurl = searchParams.get("pdfurl") as string;
-    await axios.get(pdfurl, {
-      headers: {
-        "Content-Type": "application/pdf"
-      }
-    });
-    return NextResponse.json({ok: true});
+    redirect(pdfurl);
   } catch (error) {
     console.error("Error downloading PDF:", error);
     return NextResponse.json({ok: false});
